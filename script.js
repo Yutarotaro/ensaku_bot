ad148df20127fd5aa3438cab6fa9b7cd4401a243
Yutarotaro <53333096+Yutarotaro@users.noreply.github.com>
2020-01-17 12:52:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var imgg = document.getElementById("image_place");
 var rand = 1;
 var max = 42;
 var min = 1;
+var inter = 250;
 
 // ルーレットを開始
 function showSakura() {
-    var inter = 250;
     var count = 0;
     roulette = setInterval(function() {
     var tmp = rand;
@@ -15,7 +15,6 @@ function showSakura() {
     rand = tmp;
     imgg.src = "./saku"+rand+".jpeg";
     count+=100;
-    inter += 50;
     if(count >= 1000){
         clearInterval(roulette);
         return;
