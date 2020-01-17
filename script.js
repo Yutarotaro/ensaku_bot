6192b610d6b1cb2d8f498b1def44af6b4ac3a183
Yutarotaro <53333096+Yutarotaro@users.noreply.github.com>
2020-01-17 09:54:44 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+var imgg = document.getElementById("image_place");
+var rand = 1;
+var max = 42;
+var min = 1;
+
+// ルーレットを開始
+function showSakura() {
+  
+    var count = 0;
+    var inter = 250;
+    roulette = setInterval(function() {
+    var tmp = rand;
+    while(tmp == rand){
+        tmp = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+    }
+    rand = tmp;
+    imgg.src = "saku"+rand+".jpeg";
+    count+=100;
+    if(count >= 1000){
+        clearInterval(roulette);
+    };
+    }, inter);
+    
+
+}
+ 
+      
